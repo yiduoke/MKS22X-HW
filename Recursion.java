8d20a004a9ad0370217e45327fbe3b956e631978
yiduoke <yiduoke822@jhs185.org>
2017-02-08 17:57:51 +0800

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,18 @@
+
+public class Recursion {
+	public static String name(){return "Ke,Yiduo";};
+	 public static double sqrt(double n){
+		 return sqrtHalp(n,1);
+	 }; 
+	 public static double sqrtHalp(double n, double guess){
+		if(n < 0){
+			    throw new IllegalArgumentException();}
+		if (n==0){return 0;}
+		 if (Math.abs(n-guess*guess)/n<0.000000001){return guess;}
+		 return sqrtHalp(n,(n/guess+guess)/2);
+	 }
+	 
+	 public static void main(String[] args){
+		 System.out.println(sqrt(100));
+	 }
+}
