cc586f7dc1f0e7d54aac4bc60a6d5f742656b2fd
yiduoke <yiduoke822@jhs185.org>
2017-02-13 12:16:34 -0500

    Add files via upload

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -9,7 +9,7 @@ public class QueenBoard {
 			oneSolution=new int[size][size];
 		    }
 	public void addQueen(int r, int c){
-		board[r][c]--;
+		board[r][c]--;//-1 = a queen
 		for (int x=0; x<board.length;x++){
 			for (int y=0; y<board.length; y++){
 				if (!(x==r && y== c)){//+1 to the squares it threatens
@@ -18,7 +18,7 @@ public class QueenBoard {
 			}
 		}
 	}
-	public void removeQueen(int r, int c){
+	public void removeQueen(int r, int c){//basically anti-addQueen
 		board[r][c]++;
 		for (int x=0; x<board.length;x++){
 			for (int y=0; y<board.length; y++){
@@ -28,13 +28,34 @@ public class QueenBoard {
 			}
 		}
 	}
-    public boolean solve(){
+    public void solve(){
     	solutionCount=0;
-    	return solveH(0);
+	countSolutions(0);
+    	solveH(0);
     }
-    private boolean solveH(int c){
+    public void countSolutions(int c){
     	if (c>=board.length){
     		solutionCount++;
+    		for (int r=0; r<board.length; r++){
+    			for (int cee=0; cee<board.length; cee++){
+    				oneSolution[r][cee]=board[r][cee];
+    			}
+    		}
+    		//return true;
+		return;	
+    	}
+    	for (int r=0; r<board.length; r++){
+    			if (board[r][c]==0){
+    				addQueen(r,c);
+    				countSolutions(c+1);
+    				removeQueen(r,c);
+    			}
+    		}
+    	//return solutionCount>0;
+    }
+
+	private boolean solveH(int c){
+	    	if (c>=board.length){
     		for (int r=0; r<board.length; r++){
     			for (int cee=0; cee<board.length; cee++){
     				oneSolution[r][cee]=board[r][cee];
@@ -45,14 +66,14 @@ public class QueenBoard {
     	for (int r=0; r<board.length; r++){
     			if (board[r][c]==0){
     				addQueen(r,c);
-    				solveH(c+1);
-    				removeQueen(r,c);
+				if (solveH(c+1)){
+    				solveH(c+1); return true;}
+    				else{removeQueen(r,c);}
     			}
     		}
-    	return solutionCount>0;
+	return false;
     }
     
-    //public boolean countSolutions(){}
     public int getSolutionCount(){return solutionCount;}
     public String toString(){
     	String x="";
@@ -71,10 +92,10 @@ public class QueenBoard {
     	return x;
     }
     
-  /*  public static void main(String[] args){
-    	QueenBoard marg=new QueenBoard(8);	
+    public static void main(String[] args){
+    	QueenBoard marg=new QueenBoard(13);	
     	marg.solve();
-    	System.out.println(marg.solutionCount);
-    }*/
+    	System.out.println(marg);
+    }
     
 }
