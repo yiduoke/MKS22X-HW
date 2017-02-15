8426146428c04b034d66004c3681b4288f0f7f96
yiduoke <yiduoke822@jhs185.org>
2017-02-15 12:17:04 -0500

    Add files via upload

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -19,18 +19,48 @@ public class KnightBoard{
     	return x;
     }
 
-    public void solve(){solveH(1);}
-    public boolean solveH(int num){
+    public void solve()solveH(int r, int c, 1);}
+    public boolean solveH(int r, int c, int num){
 	if (num>board.length*board[0].length){return true;}
 	for (int r=0;r<board.length;r++){
 	    for (int c=0;c<board[0].length;c++){
 		if (board[r][c]==0){
 		    addKnight(r,c,num);
-		    if (solveH(/*all up to 8 spots)*/)){
-			solveH(/*thatPlace*/);
+
+		    if (solveH(r+2,c+1,num+1)){
+			solveH(r+2,c+1,num+1);
 			return true;
-			else{removeKnight(r,c);}
 		    }
+		    if (solveH(r+1,c+2,num+1)){
+			solveH(r+1,c+2,num+1);
+			return true;
+		    }
+		    if (solveH(r-2,c-1,num+1)){
+			solveH(r-2,c-1,num+1);
+			return true;
+		    }
+		    if (solveH(r-1,c-2,num+1)){
+			solveH(r-1,c-2,num+1);
+			return true;
+		    }
+		    if (solveH(r-1,c+2,num+1)){
+			solveH(r-1,c+2,num+1);
+			return true;
+		    }
+		    if (solveH(r+2,c-1,num+1)){
+			solveH(r+2,c-1,num+1);
+			return true;
+		    }
+		    if (solveH(r+1,c-2,num+1)){
+			solveH(r+1,c-2,num+1);
+			return true;
+		    }
+		    if (solveH(r-2,c+1,num+1)){
+			solveH(r-2,c+1,num+1);
+			return true;
+		    }
+
+			else{removeKnight(r,c);}
 		}
 	    }
 	}
