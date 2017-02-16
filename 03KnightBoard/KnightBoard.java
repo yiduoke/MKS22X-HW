9f0d27fbbf481ce13f8b757859c84e57fd81e041
yiduoke <yiduoke822@jhs185.org>
2017-02-15 21:45:29 -0500

    Add files via upload

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,20 +1,24 @@
 public class KnightBoard{
     private int[][]board;
+    int xMove[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int yMove[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+    
     public KnightBoard(int startingRows, int startingCols){
 	board=new int[startingRows][startingCols];
 }
 
     public String toString(){
+    	int a=board.length;
+    	int b=board[0].length;
+    	try{if (board[0][1]==0){board=new int[a][b];}}
+    	catch(ArrayIndexOutOfBoundsException e){}
     	String x="";
     	for (int r=0;r<board.length;r++){
-    		for (int c=0;c<board.length;c++){
-    			if (c==board.length-1){
-			    x+=board[r][c]+"\n";
-    			}
-    			else{    				
-    		            x+=board[r][c]+" ";
-    				}
+    		for (int c=0;c<board[0].length;c++){ 
+    			if (board[r][c]<10){x+=" "+board[r][c]+" ";}
+    			else{x+=board[r][c]+" ";}
     		}
+    		x+="\n";
     	}
     	return x;
     }
@@ -26,88 +30,36 @@ public class KnightBoard{
 	board[r][c]=0;
     }
 
-    public void solve(){solveH (0,0,1);}
-    public boolean solveH(int x, int y, int num){
+    public void solve(){
+    	addKnight(0,0,1);
+    	solveH (0,0,2);
+    }
+    
+    public boolean solveH(int r, int c, int num){
 	if (num>board.length*board[0].length){return true;}
-	for (int r=0;r<board.length;r++){
-	    for (int c=0;c<board[0].length;c++){
-		if (board[r][c]==0){
-		    addKnight(r,c,num);
-
-		    try{
-			if (solveH(r+2,c+1,num+1)){
-			solveH(r+2,c+1,num+1);
-			return true;
+	for (int i=0; i<8; i++){
+		int nextX=r+xMove[i];
+		int nextY=c+yMove[i];
+		if (canPut(nextX,nextY)){
+			addKnight(nextX,nextY,num);
+			if (solveH(nextX,nextY,num+1)){
+				return true;
 			}
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r+1,c+2,num+1)){
-			solveH(r+1,c+2,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r-2,c-1,num+1)){
-			solveH(r-2,c-1,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r-1,c-2,num+1)){
-			solveH(r-1,c-2,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r-1,c+2,num+1)){
-			solveH(r-1,c+2,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r+2,c-1,num+1)){
-			solveH(r+2,c-1,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r+1,c-2,num+1)){
-			solveH(r+1,c-2,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
-		    try{
-		    if (solveH(r-2,c+1,num+1)){
-			solveH(r-2,c+1,num+1);
-			return true;
-		    }
-	else{removeKnight(r,c);}
-		    }
-			catch (ArrayIndexOutOfBoundsException e){}
+			else{removeKnight(nextX,nextY);}
 		}
-	    }
 	}
 	return false;
     }
 
+    private boolean canPut(int r, int c){
+        if (r >= 0 && r < board.length && c >= 0 && c < board[0].length && board[r][c] == 0){
+        	return true;}
+        return false;
+    }
     
     public static void main(String[] args){
-    	KnightBoard marg=new KnightBoard(7,7);	
-	marg.solve();
+    	KnightBoard marg=new KnightBoard(7,9);	
+    	marg.solve();
     	System.out.println(marg);
     }
 }
