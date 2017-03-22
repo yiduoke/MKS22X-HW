71ff406e84bc8f472f853d7cabac6cd820dc7bc8
yiduoke <yiduoke822@jhs185.org>
2017-03-22 09:08:45 +0800

    done

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,76 +1,67 @@
 public class Quick {
 
-    public static void quickSortDutch(int data[], int start, int end){
+	//helper for quicksort
+    public static void quicksort(int data[], int start, int end){
 	if (start<end){
-	    int lt=partitionDutch(data,start,end)[0];
-	    int gt=partitionDutch(data,start,end)[1];
-	    quickSortDutch(data,start,lt-1);
-	    quickSortDutch(data,gt+1,end);
+	    int lt=partition(data,start,end)[0];
+	    int gt=partition(data,start,end)[1];
+	    quicksort(data,start,lt-1);
+	    quicksort(data,gt+1,end);
 	}
     }
-
-    public static void quickSortDutch(int data[]){quickSortDutch(data,0,data.length-1);}
+    
+    //wrapper for quicksort
+    public static void quicksort(int data[]){quicksort(data,0,data.length-1);}
 	
+    //helper for quickselect
 	public static int quickselect(int[] data, int start, int end, int k){
-			/*int pivotIndex=partition(data,start,end);
-			if (pivotIndex==k){return data[k];}
-			if (pivotIndex<k){return quickselect(data,pivotIndex+1,end,k);}
-			return quickselect(data,start,pivotIndex-1,k);*/
-			int lt=partitionDutch(data,start,end)[0];
-			int gt=partitionDutch(data,start,end)[1];
+			int lt=partition(data,start,end)[0];
+			int gt=partition(data,start,end)[1];
 			if (k<=gt && k>=lt){return data[k];}
 			else if (gt<k){return quickselect(data,gt+1,end,k);}
 			return quickselect(data,start,lt-1,k);
 			
 	}
+	
+	//wrapper for quickselect
     public static int quickselect(int[] data, int k){return quickselect(data,0,data.length-1,k);}
 	
-	public static String toString(int[] data){
+    //for testing
+	private static String toString(int[] data){
 		String x="";
 		for (int i=0; i<data.length; i++){
 			x+=data[i]+" ";
 		}
 		return x;
 	}
-
-	public static int[] partitionDutch(int[] data){return partitionDutch(data,0,data.length-1);}
-    public static int[] partitionDutch(int[] data, int start, int end){
+	
+	//used in quicksort and quickselect; a helper method
+    public static int[] partition(int[] data, int start, int end){
 	int i=start;
 	int lt=start;
 	int gt=end;
 	while (i<=gt){
 	    if (data[i]==data[start]){
-		i++;}
+	    	i++;
+	    }
 	    else if (data[i]<data[start]){
-		swap(data, i, lt);
-		lt++;
-		i++;
+	    	swap(data, i, lt);
+	    	lt++;
+	    	i++;
 	    }
 	    else{
-		swap(data, i, gt);
-		gt--;
+	    	swap(data, i, gt);
+	    	gt--;
 	    }
-	}
+	    }
 	int[] returnee={lt,gt};
 	return returnee;
-}
+    }
 
+    //a helper method for partition
     public static void swap(int[] data, int x, int y){
 	int temp = data[y];
 	data[y] = data[x];
 	data[x] = temp;
 }
-	  
-	public static void main(String[] args){
-	    int[] margaret = {10,1,-2,3,-6,4,5,6,-4,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-		for (int i=0; i<100; i++){
-			for (int j=0; j<30; j++){
-				margaret[j]=(int)(Math.random()*400);
-			}
-			System.out.println(quickselect(margaret,5));
-			//quickSortDutch(margaret);
-			System.out.println(margaret[5]);
-			//System.out.println(toString(margaret));
-		}
-	}	
 }
