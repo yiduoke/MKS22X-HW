97edf549a50da10d7f0d82ae17582584f5ca40c1
yiduoke <yiduoke822@jhs185.org>
2017-03-24 00:26:09 +0800

    Add files via upload

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -11,5 +11,11 @@ public class Merge{
 	for (int i=ary.length/2; i<ary.length; i++){
 	    right[i-ary.length/2]=ary[i];
 	}
+	mergesort(left);
+	mergesort(right);
+	merge(left,right,ary);
+    }
+    public static void main(String[] args){
+	System.out.println(5/2);
     }
 }
