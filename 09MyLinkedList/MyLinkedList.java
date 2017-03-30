17c99fe94d8f7469359ca94f13f86fcce63cdf11
yiduoke <yiduoke822@jhs185.org>
2017-03-30 17:23:57 +0800

    Add files via upload

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,6 +1,8 @@
 public class MyLinkedList{
+	
     LNode start;
     int size;
+    
     //inner city--------------------------
     class LNode{
 	int first;
@@ -14,19 +16,43 @@ public class MyLinkedList{
 	}
     }
     //inner city--------------------------
+    
     public MyLinkedList(){
-	start=null;
-	size=0;
+    	start=null;
+    	size=0;
     }
     public boolean add(int value){
-	
-	return true;
+    	size++;
+    	LNode current = start;
+    	for (int i=0; i<size; i++){
+    		start=start.next;
+    	}
+    	start.first=value;
+    	start.next=null;
+    	return true;
     }
+    
     public int size(){return size;}
-    public String toString(){}
+    
+    public String toString(){
+    	String x="[";
+    	LNode current = start;
+    	for (int i=0; i<size; i++){
+    		if (i==size-1){x+=current.first;}
+    		x+=current.first+", ";
+    		current=current.next;
+    	}
+    	x+="]";
+    	return x;
+    }
     public int get (int index){}
     public int set(int index, int newValue){}
     public int indexOf(int value){}
     public boolean add(int index, int value){}
     public int remove(int index){}
+    
+    public static void main(String[] args){
+    	MyLinkedList margaret = new MyLinkedList();
+    	System.out.println(margaret.toString());
+    }
 }
