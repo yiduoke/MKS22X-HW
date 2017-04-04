3d498a278a3482dbe1e21b285f1d29c295c78ee5
yiduoke <yiduoke822@jhs185.org>
2017-04-04 00:25:31 -0400

    is not doubly linked

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,12 +1,14 @@
 public class MyLinkedList{
 	
     LNode start;
+    LNode tail;
     int size;
     
     //inner city--------------------------
     class LNode{
 	int first;
 	LNode next=null;
+	LNode prev=null;
 	public LNode(int x, LNode y){
 	    first=x;
 	    next=y;
@@ -21,32 +23,33 @@ public class MyLinkedList{
     	start=null;
     	size=0;
     }
-    public boolean add(int value){
-    	 LNode last = lastNode();
-         if (last == null) {
+    public boolean add(int value){//doubly linked
+         if (start == null) {
              start = new LNode(value);
          } 
          else{
-             last.next = new LNode(value);
+             tail=new LNode(value);
+             getNode(size-1).next=tail;
+             tail.prev=getNode(size-1);
          }
          size++;
          return true;
     }
     
-    public LNode lastNode() {
-        if (start == null) {
-            return null;
-        }
-        LNode current = start;
-        while (current.next != null) {
-            current = current.next;
-        }
-        return current;
+    private LNode getNode(int index){//new
+    	if (start==null){
+    		return null;
+    	}
+    	LNode current=start;
+    	for (int i=0; i<index; i++){
+    		current=current.next;
+    	}
+    	return current;
     }
     
     public int size(){return size;}
     
-    public String toString(){
+    public String toString(){//won't change
     	String x="[";
     	LNode current = start;
     	for (int i=0; i<size; i++){
@@ -60,7 +63,7 @@ public class MyLinkedList{
     	return x;
     }
     
-    public int get (int index){
+    public int get(int index){//won't change
     	LNode current=start;
     	if (start == null){return 0;}
     	for (int i=0; i<index; i++){
@@ -69,94 +72,72 @@ public class MyLinkedList{
     	return current.first;
     }
     
-    public int set(int index, int newValue){
-    	LNode current=start;
+    public int set(int index, int newValue){//modified
     	if (start == null){start = new LNode(newValue);}
-    	for (int i=0; i<index; i++){
-    		current = current.next;
-    	}
-    	int x = current.first;
-    	current.first = newValue;
+    	int x = getNode(index).first;
+    	getNode(index).first = newValue;
     	return x;
     }
     
-    public int indexOf(int value){
-    	LNode current=start;
+    public int indexOf(int value){//modified
     	if (start == null){return -1;}
     	for (int i=0; i<size; i++){
-    		if (current.first == value) {return i;}
-    		current = current.next;
+    		if (getNode(i).first==value){return i;}
     	}
     	return -1;
     }
     
-    public void add(int index, int value){
-    	size++;
-    	if (index == 0){
-    		LNode temp = start;
-    		start=new LNode(value,temp);
+    public void add(int index, int value){//doubly linked
+    	LNode thing=new LNode(value);
+    	if (index==0){
+    		start.prev=thing;
+    		thing.next=start;
+    		start=thing;
+    	}
+    	else if(index==size){
+    		tail.next=thing;
+    		thing.prev=tail;
+    		tail=thing;
     	}
     	else{
-    	LNode current=start;
-    	for (int i=0; i<index; i++){
-    		current = current.next;
+    		thing.prev=getNode(index-1);
+    		thing.next=getNode(index);
+    		getNode(index).prev=thing;
+    		getNode(index-1).next=thing;
     	}
-    	LNode temp = current;
-    	
-    	LNode current2 = start;
-    	for (int i=0; i<index-1; i++){
-    		current2 = current2.next;
-    	}
-    	current2.next = new LNode(value, temp);
-    	}
+    	size++;
     }
     
-    public int remove(int index){
-    	if (index == 0){
-    		int x = start.first; 
-    		start = start.next; 
-    		size--;
-    		return x;
-    	}
-    	
-    	else if (index == size-1){
-            LNode current = start;
-            while (current.next != null) {
-                current = current.next;
-            }
-            int x=current.first;
-            current=null;
-            size--;
-            return x;
-    	}
-    	
+    public int remove(int index){//doubly linked
+    	if (index<0 || index>=size){throw new IllegalArgumentException();}
+    	int x=getNode(index).first;
+    	if (index==0){ 
+    		start=start.next;
+    		start.prev=null;
+    		}
+    	else if (index==size-1){
+    		tail=tail.prev;
+    		tail.next=null;
+    		}
     	else{
-    		size--;
-    		LNode current=start;
-    		for (int i=0; i<index+1; i++){
-    			current = current.next;
-    		}
-    		LNode temp = current;
-    		int x=current.first;
-    	
-    		LNode current2 = start;
-    		for (int i=0; i<index-1; i++){
-    			current2 = current2.next;
-    		}
-    		current2.next = temp;
-    		return x;
+    		LNode y=getNode(index);
+    		y.prev.next=y.next;
+    		y.next.prev=y.prev;
     	}
+    	size--;
+    	return x;
     }
     
     public static void main(String[] args){
     	MyLinkedList margaret = new MyLinkedList();
     	margaret.add(10);
     	margaret.add(5);
+    	//margaret.add(9);
     	margaret.add(7);
     	margaret.add(11);
-    	margaret.add(2,9); //[10, 5, 9, 7, 11]
-    	margaret.remove(0);
+    	margaret.add(2,9);//[10, 5, 9, 7, 11]
+    	margaret.remove(4);
     	System.out.println(margaret.toString());
-    	System.out.println(margaret.indexOf(9));
+    	System.out.println(margaret.indexOf(11));
     }
 }
