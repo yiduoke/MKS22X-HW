01899b6106580c7bd6a1a51f5639cc0bf57bc244
yiduoke <yiduoke822@jhs185.org>
2017-04-18 12:02:02 -0400

    Add files via upload

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -3,48 +3,55 @@ public class MyDeque{
     private int end;
     private int size;
     private String[] deck;
-
-    public MyDeque(){
-	deck=new String[10];
-	start=0;
-    }
-
-    public void addFirst(String x){
-	String[] temp;
-	if (size==deck.length){
-	    temp=new String[deck.length*2];
-	}
-        else{temp=new String[deck.length];}
-	    for (int i=1; i<deck.length; i++){
-		temp[i]=deck[i];
-	    }	    deck=temp;
-	    deck[0]=x;
-	    size++;
-    }
-
-    public void addLast(String x){
-	if ((end+1%)deck.length!=start){
-	    deck[(end+1)%deck.length]=x;
-	}
-	else{
-	    String[] temp=new String[deck.length*2];
-	    for (int i=0; i<deck.length; i++){
-		temp[i]=deck[i];
-	    }
-	    deck=temp;
-	    deck[end+1]=x;
-	}
-	size++;
-    }
-
-    public String removeLast(){
-	last=(last-1)%deck.length;
-
-    }
-    public String removeFirst(){
-	first=(first+1)%deck.length;
-    }
     
+    public MyDeque(){deck=new String[10];}
 
+   public void addFirst(String x){
+	   if (size==deck.length){
+		   expand();
+		   start=deck.length-1;
+	   }
+	   else{
+		   start=(start-1)%deck.length;
+	   }
+	   deck[start]=x;
+	   size++;
+   }
     
+   public void addLast(String x){
+	   if (size==deck.length){
+		   expand();
+		   end=size;
+	   }
+	   else{
+		   end=(end+1)%deck.length;
+	   }
+	   deck[end]=x;
+	   size++;
+   }
+   
+   private void expand(){
+	   String[] temp=new String[deck.length*2];
+	   for (int i=0; i<size; i++){
+		  temp[i]=deck[(start+i)%deck.length];
+	   }
+   }
+   
+   public String removeFirst(){
+	   String x=deck[start];
+	   size--;
+	   start=(start+1)%deck.length;
+	   return x;
+   }
+   
+   public String removeLast(){
+	   String x=deck[end];
+	   size--;
+	   end=(end-1)%deck.length;
+	   return x;
+   }
+   
+   public String getFirst(){return deck[start];}
+   
+   public String getLast(){return deck[end];}
 }
