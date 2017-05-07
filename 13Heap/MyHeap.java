a905cc464228b0cf4c45581d1d558ddedc757320
yiduoke <yiduoke822@jhs185.org>
2017-05-06 21:15:16 -0400

    Add files via upload

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,59 +1,121 @@
-public class MyHeap{
-    private String[] arr;
-    private int[] counts;
-    private boolean max;
-    private int size;
-    
-    public MyHeap(){
-	arr=new String[10];
-    }
+import java.util.ArrayList;
 
-    public MyHeap(boolean x){
-	if (x){
-	    max=true;
+public class MyHeap {
+	private ArrayList<String> list;
+	private boolean max;
+	private int size;
+	
+	public MyHeap(){
+		list=new ArrayList<String>();
 	}
-    }
-
-    public void add(String x){
-	if (max){
-	    try{
-		arr[size+1]=x;
-		for (int i=size+1; i>0; i/=2){
-		    if (x.compareTo(arr[i])>0){		
-			arr[i]=arr[i/2];
-		    }
+	
+	public MyHeap(boolean x){
+		if (x){
+			max=true;
 		}
-	    }
-	    catch (ArrayIndexOutOfBoundsException e){
-		grow();
-		for (int i=size+1; i>0; i/=2){
-		    arr[i]=arr[i/2];
-		}
-	    }
+		list=new ArrayList<String>();
 	}
-	else {
-	    try{
-		arr[size+1]=x;
-		for (int i=size+1; i>0; i/=2){
-		    if (x.compareTo(arr[i])<0){
-			arr[i]=arr[i/2];
-		    }
+	
+	public void add(String x){
+		if (size==0){
+			list.add("");
+			list.add(x);
+			size++;
+			return;
 		}
-	    }
-	    catch (ArrayIndexOutOfBoundsException e){
-		grow();
-		for (int i=size+1; i>0; i/=2){
-		    arr[i]=arr[i/2];
+		else{
+			list.add(x);
+			size++;
 		}
-	    }
+		int i=size;
+		if (max){
+			while (list.get(i).compareTo(list.get(i/2))>0 && i>1){
+				swap(i,i/2);
+				i/=2;
+			}
+		}
+		else{
+			while (list.get(i).compareTo(list.get(i/2))<0 && i>1){
+				swap(i,i/2);
+				i/=2;
+			}
+		}
 	}
-    }
-
-    public String remove(){
-	String x=arr[1];
-	for (int i=1; i<=size; i*=2){
-	    
+	
+	public String remove(){
+		String x=list.get(1);
+		int i=1;
+		if (max){
+			while (i<=size/2){
+				if (i==size/2 && size%2==0){
+					list.set(i,list.get(i*2));
+					size--;
+					return x;
+				}
+				if (list.get(i*2).compareTo(list.get(i*2+1))>=0){
+					list.set(i, list.get(i*2));
+					i*=2;
+				}
+				else{
+					list.set(i, list.get(i*2+1));
+					i=i*2+1;
+				}
+				//list.set(i, list.get(i*2));	
+			}
+		}
+		else{
+			while (i<=size/2){
+				if (i==size/2 && size%2==0){
+					list.set(i,list.get(i*2));
+					size--;
+			    	return x;
+				}
+				if (list.get(i*2).compareTo(list.get(i*2+1))<=0){
+					list.set(i, list.get(i*2));
+					i*=2;
+				}
+				else{
+					list.set(i, list.get(i*2+1));
+					i=i*2+1;
+				}
+				//list.set(i, list.get(i*2));
+				
+			}
+		}
+		size--;
+		return x;
 	}
-    }
+	
+	public String peek(){
+		return list.get(1);
+	}
+	
+	public String toString(){
+		String x="";
+		for (int i=1; i<=size; i++){
+			x+=list.get(i)+" ";
+		}
+		return x;
+	}
+	
+	private void swap(int x, int y){
+		String a=list.get(x);
+		list.set(x, list.get(y));
+		list.set(y, a);
+	}
+	
+	public static void main(String[] args){
+		MyHeap margaret=new MyHeap(false);
+		margaret.add("B");
+		margaret.add("C");
+		margaret.add("D");
+		margaret.add("C");
+		margaret.add("A");
+		margaret.add("A");
+		margaret.add("F");
+		margaret.add("Y");
+		System.out.println(margaret);
+		System.out.println(margaret.remove());
+		System.out.println(margaret);
+	}
 }
-
