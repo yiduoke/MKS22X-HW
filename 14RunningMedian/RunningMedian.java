6602eb0764f960a62e94b1b0d2087ab489c95ba7
yiduoke <yiduoke822@jhs185.org>
2017-05-08 12:30:51 -0400

    Add files via upload

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,40 @@
+public class RunningMedian{
+
+    private MyHeap min;
+    private MyHeap max;
+
+    public RunningMedian(){
+	min=new MyHeap(false);
+	max=new MyHeap(true);
+    }
+
+    public void add(int x){
+	if (x<getMedian()){
+	    max.add(x);
+	}
+	else{
+	    min.add(x);
+	}
+    }
+
+    public double getMedian(){
+	if (min.size()==0 && max.size()==0){
+	    return 0;
+	}
+	else if (min.size()==0){
+	    return max.peek();
+	}
+	else if (max.size()==0){
+	    return min.peek();
+	}
+	else{
+	    return (min.peek()+max.peek())/2;
+	}
+    }
+
+    public static void main(String[] args){
+	RunningMedian margaret=new RunningMedian();
+	System.out.println(margaret.getMedian());
+    }
+
+}
