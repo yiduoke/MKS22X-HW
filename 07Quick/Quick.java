7cfcbe723e222dc248499e87aa2a4dd3bc7db4b7
yiduoke <yiduoke822@jhs185.org>
2017-06-02 15:38:07 -0400

    MR K. I FIXED IT PLEASE GRADE IT AGAIN THANK YOU!!

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,31 +1,87 @@
 public class Quick {
-
-	//helper for quicksort
-    public static void quicksort(int data[], int start, int end){
-	if (start<end){
-	    int lt=partition(data,start,end)[0];
-	    int gt=partition(data,start,end)[1];
-	    quicksort(data,start,lt-1);
-	    quicksort(data,gt+1,end);
-	}
-    }
     
     //wrapper for quicksort
     public static void quicksort(int data[]){quicksort(data,0,data.length-1);}
 	
     //helper for quickselect
 	public static int quickselect(int[] data, int start, int end, int k){
-			int lt=partition(data,start,end)[0];
-			int gt=partition(data,start,end)[1];
-			if (k<=gt && k>=lt){return data[k];}
-			else if (gt<k){return quickselect(data,gt+1,end,k);}
-			return quickselect(data,start,lt-1,k);
-			
+		if (k<=end && k>=start){return data[k];}
+		else{
+		    int pivotIndex=(int)(Math.random() * (end - start + 1) + start);	
+		    int pivot=data[pivotIndex];
+		    swap(data,pivotIndex,start);
+		
+		    int lt=start+1;
+		    int gt=end;
+		    int i=start+1;
+		    while (i<=gt){
+		    	if (data[i]==pivot){
+		    		i++;
+	    	}
+		    	else if (data[i]<pivot){
+		    		swap(data, i, lt);
+		    		lt++;
+		    		i++;
+		    	}
+		    	else{
+		    		swap(data, i, gt);
+		    		gt--;
+		    	}
+		    }
+			pivotIndex=lt-1;
+			swap(data,start,pivotIndex);
+			if (k>gt){
+				return quickselect(data,k,gt,end);
+			}
+			else{
+				return quickselect(data,k,start,lt);
+			}
+		}
+		
 	}
 	
 	//wrapper for quickselect
     public static int quickselect(int[] data, int k){return quickselect(data,0,data.length-1,k);}
 	
+    public static void quicksort(int[] data, int start, int end){
+	    if (end-start<2){
+	    	return;
+	    }
+	    
+	    int pivotIndex=(int)(Math.random() * (end - start + 1) + start);	
+	    int pivot=data[pivotIndex];
+		swap(data,pivotIndex,start);
+		
+	    int lt=start+1;
+	    int gt=end;
+	    int i=start+1;
+	    while (i<=gt){
+	    	if (data[i]==pivot){
+	    		i++;
+	    	}
+	    	else if (data[i]<pivot){
+	    		swap(data, i, lt);
+	    		lt++;
+	    		i++;
+	    	}
+	    	else{
+	    		swap(data, i, gt);
+	    		gt--;
+	    	}
+	    }
+	    
+	    swap(data,start,lt-1);
+	    quicksort(data,start,lt-1);
+	    quicksort(data, i-1,end);
+    }
+
+    //a helper method for partition
+    public static void swap(int[] data, int x, int y){
+    	int temp = data[y];
+    	data[y] = data[x];
+    	data[x] = temp;
+    }
+    
     //for testing
 	private static String toString(int[] data){
 		String x="";
@@ -34,37 +90,4 @@ public class Quick {
 		}
 		return x;
 	}
-	
-	//used in quicksort and quickselect; a helper method
-    public static int[] partition(int[] data, int start, int end){
-	int pivotIndex=(int)(Math.random() * (end - start + 1) + start);	         swap(data,pivotIndex,start);
-
-
-	int i=start;
-	int lt=start;
-	int gt=end;
-	while (i<=gt){
-	    if (data[i]==data[start]){
-	    	i++;
-	    }
-	    else if (data[i]<data[start]){
-	    	swap(data, i, lt);
-	    	lt++;
-	    	i++;
-	    }
-	    else{
-	    	swap(data, i, gt);
-	    	gt--;
-	    }
-	    }
-	int[] returnee={lt,gt};
-	return returnee;
-    }
-
-    //a helper method for partition
-    public static void swap(int[] data, int x, int y){
-	int temp = data[y];
-	data[y] = data[x];
-	data[x] = temp;
-}
 }
